d0211a82809cc083eaa3a5cfbee9ce5ac57278a5
Lei-Ann <22-30223@g.batstate-u.edu.ph>
2023-11-08 13:57:01 +0800

    feat/insert-10-rows-animals

--- veterinary.sql
+++ veterinary.sql
@@ -60,6 +60,20 @@ CREATE TABLE medicalrecords(
         FOREIGN KEY (animalid) REFERENCES animals(animalid),
         FOREIGN KEY (doctorid) REFERENCES doctors(doctorid)
 );
+INSERT INTO 
+    animals(animalid, name, species, breed, dateofbirth, gender, color, ownerid)
+VALUES
+    (1, 'Fido', 'Dog', 'Golden Retriever', '2018-03-15', 'Male', 'Golden', 1),
+    (2, 'Whiskers', 'Cat', 'Siamese', '2019-07-20', 'Female', 'Cream', 2),
+    (3, 'Rocky', 'Dog', 'German Sheperd', '2017-05-10', 'Male', 'Black and Tan', 3),
+    (4, 'Fluffy', 'Cat', 'Persian', '2020-01-30', 'Male', 'White', 4),
+    (5, 'Luna', 'Dog', 'Labrador Retriever', '2019-11-25', 'Female', 'Yellow',5),
+    (6, 'Mochi', 'Cat', 'Maine Coon', '2018-09-12', 'Male', 'Brown Tabby', 6),
+    (7, 'Bella', 'Dog', 'Poodle', '2020-04-05', 'Female', 'White', 7),
+    (8, 'Simba', 'Cat', 'Bengal', '2018-06-18', 'Male', 'Spotted Rosetted', 😎,
+    (9, 'Max', 'Dog', 'Dachshuhd', '2020-07-14', 'Male', 'Black and Tan', 9),
+    (10, 'Cleo', 'Cat', 'Ragdoll', '2019-12-22', 'Female', 'Seal Point', 10);
+
 
 INSERT INTO 
     owners(ownerid,ofirstname,olastname,address,phone,email)
